716af4dc80e5222afa103e6633b2fdd1bff4099c
Samuel Adonay Ortiz Carballo <00023020@uca.edu.sv>
2024-09-23 01:23:06 -0600

    :sparkles: Add file operations functions

---
+++ src/utils/fileOperations.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import path from 'path';
+import { Expense } from '../interfaces/expense.interface';
+
+const PATH_TO_JSON: string = path.join(__dirname, '..', 'expenses.json');
+
+function readDataBase(PATH_TO_JSON): Expense[] {
+    let expenses: Expense[] = [];
+    if(!fs.existsSync(PATH_TO_JSON)){
+        fs.writeFileSync(PATH_TO_JSON, '[]', 'utf8');
+    }
+    let data = fs.readFileSync(PATH_TO_JSON, 'utf-8');
+    expenses = JSON.parse(data) as Expense[];
+    return expenses;
+}
+
+function writeDataBase(data: Expense[]){
+    const updatedExpenses = JSON.stringify(data, null, 2);
+    fs.writeFileSync(PATH_TO_JSON, updatedExpenses, 'utf-8');
+}
