a1583f5852f9cacc87eeb80ea6b8557b4b7f31ac
Samuel Ortiz <00023020@uca.edu.sv>
2024-09-26 11:49:26 -0600

    :recycle: Refactor readDatabase function to use Date data type

--- src/commands/expense/summaryExpense.ts
+++ src/commands/expense/summaryExpense.ts
@@ -11,6 +11,21 @@ function summaryExpenses(month: number){
         console.log(`The summary for all the expenses is $${summary}`);
         return true;
     }
+    const filteredExpenses: Expense[] = allExpenses.filter((expense: Expense) => {
+        return expense.date.getMonth() + 1 === month;
+    })
+    
+    if(filteredExpenses.length===0){
+        console.log(`The month ${month} doesn't have any expenses yet`);
+        return false;
+    }
+    
+    filteredExpenses.forEach((expense: Expense) => {
+        summary += expense.amount;
+    })
+
+    console.log(`The total expenses of the month ${month} were $${summary}`);
+    return true;
     
 }
 
